4b88d363711e5bd6d670b862caae024d7009546d
Ecabott <ayush.bhusal0@gmail.com>
2023-08-01 14:50:30 +0545

    using bootstrap button

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ButtonComponent from "./components/Button";
+import Title from "./components/Title";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Title />
     <App />
+    <ButtonComponent />
   </React.StrictMode>
 );
