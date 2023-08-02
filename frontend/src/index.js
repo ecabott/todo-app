5dc88eeee770ff99e457abfab4da8c34850f90ac
Ecabott <ayush.bhusal0@gmail.com>
2023-08-02 13:40:33 +0545

    updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Example3 from "./components/Example3";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
