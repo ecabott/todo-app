9d5eee9d9291f8c63d2bf39f2a76d2de75891c8d
Ecabott <ayush.bhusal0@gmail.com>
2023-08-02 14:20:50 +0545

    Logican && Operator

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import Title from "./components/Title";
-import Example from "./components/Example";
-import Example2 from "./components/Example2";
-import Example3 from "./components/Example3";
+import Example from "./components/examples/Example";
+import Example2 from "./components/examples/Example2";
+import Example3 from "./components/examples/Example3";
+import Example4 from "./components/examples/Example4";
+import Example5 from "./components/examples/Example5";
+import Example6 from "./components/examples/Example6";
+
+const cars = ["Bugatti", "BMW", "Porshe"];
 
 function App() {
   return (
@@ -10,6 +15,7 @@ function App() {
       <Example />
       <Example2 />
       <Example3 isGoal={true} />
+      <Example4 cars={cars} />
     </>
   );
 }
