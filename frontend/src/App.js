445671ee1cd4e06aab426c819b87aa0b9787277f
Ecabott <ayush.bhusal0@gmail.com>
2023-08-03 15:11:56 +0545

    created initial todo app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Title from "./components/Title";
+import CustomHook from "./components/CustomHook";
+
+function App() {
+  return (
+    <>
+      <Title name="Todo App" />
+      <CustomHook />
+    </>
+  );
+}
+
+export default App;
