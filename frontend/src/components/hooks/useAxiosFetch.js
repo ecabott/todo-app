c677567def3a12a9bcd9345b8927dd0b9d5417f4
Ecabott <ayush.bhusal0@gmail.com>
2023-08-03 15:17:06 +0545

    added axios fetch

---
+++ frontend/src/components/hooks/useAxiosFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useAxiosFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        const { data } = await axios(url);
+        if (isMounted && data) {
+          setLoading(false);
+          setData(data);
+        }
+      } catch (error) {
+        setLoading(false);
+        setError(error);
+      }
+    };
+    fetchData();
+    return () => {
+      isMounted = false;
+    };
+  }, [url]);
+  return { data, loading, error };
+};
+
+export default useAxiosFetch;
