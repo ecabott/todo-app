88600baacb9acc8d46b6ece1c275e95d1c6af819
Ecabott <ayush.bhusal0@gmail.com>
2023-08-03 15:11:03 +0545

    updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ExampleApp from "./ExampleApp";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ExampleApp />
+    <App />
   </React.StrictMode>
 );
