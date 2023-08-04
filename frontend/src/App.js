a13ade5b982e2f8d836bda4f5ca4fbcb8d6c2562
Ecabott <ayush.bhusal0@gmail.com>
2023-08-04 22:26:14 +0545

    Add new components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
+import { Container, Row, Col } from "react-bootstrap";
 import Title from "./components/Title";
-import CustomHook from "./components/CustomHook";
+import Todo from "./pages/Todo";
+import AddTodo from "./components/AddTodo";
+import { API_SERVER } from "./constants";
 
 function App() {
   return (
     <>
-      <Title name="Todo App" />
-      <CustomHook />
+      <Container>
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Title name="TODO App" />
+            <AddTodo
+              label="Add New Task"
+              placeholder="Eg: Do Laundry"
+              url={`${API_SERVER}/todos`}
+            />
+            <Todo />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
